2520c73c55cf353136026e11c1aef26bdbd381d3
Sebas16608 <asebasrr444@gmail.com>
2026-09-11 21:53:09 -0600

    chore: remove unused import

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.password_validation import validate_password
 from django.db import transaction
-from django.utils.text import slugify
+
 from rest_framework import serializers
 
 from tenants.models import Membership, Tenant
